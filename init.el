d03c210eba926c1959741570a9ea1472ce6a031a
cerpins <52047345+cerpins@users.noreply.github.com>
2024-03-02 16:05:50 +0200

    update theme, finally configure lsp to not be annoying

--- init.el
+++ init.el
@@ -17,13 +17,19 @@
 
 ;; Setup LSP
 (use-package company
+  :init
+  (setq company-minimum-prefix-length 1)
   :config
   (global-set-key (kbd "C-. .") #'company-complete)
   (global-company-mode))
 (use-package lsp-mode
   :init
   (setq lsp-keymap-prefix "C-.")
-  (setq lsp-enable-snippet nil)
+  :commands (lsp-describe-thing-at-point)
+  :config
+  (global-set-key (kbd "C-. /") #'lsp-describe-thing-at-point)
+
+  ;;(setq lsp-enable-snippet nil)
   ;;:custom
   ;;(lsp-typescript-format-enable . nil)
   ;;(lsp-javascript-format-enable . nil)
@@ -37,11 +43,32 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands (lsp))
 (use-package lsp-ui
+  :init
+  ;; Signature
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  ;; Headerline
+  (setq lsp-headerline-breadcrumb-enable t)
+  ;; Lsp doc - Pretty annoying, we use 'lsp-describe-thing-at-point' instead
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-doc-show-with-cursor nil)
+  ;; Lens
+  (setq lsp-lens-enable nil)
+  ;; Modeline
+  (setq lsp-modeline-code-actions-enable nil)
+  ;; Sideline options
+  (setq lsp-ui-sideline-enable t)
+  (setq lsp-ui-sideline-show-code-actions nil)
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-show-hover nil)
+  (setq lsp-ui-sideline-show-symbol t)
   :commands (lsp-ui-mode))
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 (use-package which-key
   :config
-  (which-key-mode))
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
 (use-package flycheck
   :init
   (global-flycheck-mode)
@@ -66,24 +93,37 @@
 (desktop-save-mode 1)
 
 ;; Fonts
-(add-to-list 'custom-theme-load-path "c:/Users/unoce/Documents/emacs")
-(eval-after-load "timu-macos"
-(customize-set-variable 'timu-macos-flavour "dark"))
-(load-theme 'timu-macos t)
 (set-frame-font "-outline-Source Code Pro-regular-normal-normal-mono-16-*-*-*-c-*-iso10646-1")
+
+
+;; Themes
+;; (eval-after-load "timu-macos"
+;;  (customize-set-variable 'timu-macos-flavour "dark"))
+;; (load-theme 'timu-macos t)
+(add-to-list 'custom-theme-load-path "c:/Users/unoce/Documents/emacs")
+(use-package naysayer-theme)
+(load-theme 'naysayer t)
+
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" default))
  '(lsp-html-format-content-unformatted nil)
  '(lsp-html-format-enable nil)
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter flychecka flycheck which-key rust-mode lsp-ui lsp-treemacs company)))
+   '(naysayer-theme zenburn-theme zenburn tree-sitter-langs tree-sitter flychecka flycheck which-key rust-mode lsp-ui lsp-treemacs company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+
