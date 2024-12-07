8c281277216379e99715f99695d74d5deb080284
cerpins <52047345+cerpins@users.noreply.github.com>
2024-12-07 16:19:24 +0200

    theme

--- .init.custom.el
+++ .init.custom.el
@@ -5,13 +5,25 @@
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist '(("." . "~/.emacs.d/backup/")))
  '(compilation-disable-input t)
+ '(custom-safe-themes
+   '("5267955ef9a9e8bc64cadd113c1b15166c352ceb1798d18bab97e7892db5866f" "d46cb99f59fb858b11cac130f3a53688cc4323f3ec4aae80688bdd5d00312b91" default))
  '(package-selected-packages
    '(mince-theme smex rust-mode naysayer-theme ido-completing-read+ company))
  '(rust-always-locate-project-on-open t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
 
+; how to make a good theme?
+;; choose a good primary color for the 'default' foreground
+;;; should be easy to tell apart when there's lots of it (contrast)
+;;; should not be vibrant so it doesn't hurt to look at
+;;; tl;dr - contrast but not vibrant
+
+;; choose a background that makes the 'default' foreground contrasted
+;;; background should not be too dark or it becomes vibrant
+
+;; keywords - not sure, probably best to have something similar to foreground
+;; but of a different tint? try it
+
+;; choose a satured and light color for types
+;; choose a satured and dim color for comments
+
+;; macros and strings can be vibrant
--- init.el
+++ init.el
@@ -20,10 +20,10 @@
 (use-package company)
 
 ;; Fonts, themes
-(package-install-file "~/.emacs.d/mince-theme.el")
+;;(package-install-file "~/.emacs.d/mince-theme.el")
 (set-frame-font "-outline-Source Code Pro-regular-normal-normal-mono-16-*-*-*-c-*-iso10646-1")
 ;;(load-theme 'naysayer t)
-(load-theme 'mince t)
+;;(load-theme 'mince t)
 
 ;; No bell, topbar, menubar
 (setq visible-bell 1)
@@ -63,5 +63,55 @@
 
 (global-set-key (kbd "C-c c") 'recompile)
 ;; (global-set-key (kbd "C-c d") 'dired-all)
+
+
+
+
+;;;;; CUSTOM THEME
+
+(setq
+ mince-fg1 "#aea294"
+ mince-vars "#a5bbbf"
+ mince-mid "#dae9ea"
+ mince-fn "#dae9ea"
+ mince-bg1 "#191b1b"
+ mince-types "#69bac9"
+ mince-comment "#628790"
+ mince-macro "#a16e55"
+ mince-string "#d9e5e5"
+ )
+
+
+(custom-set-faces
+ ;; general
+ `(default ((t (:foreground ,mince-fg1  :background ,mince-bg1))))
+ `(font-lock-function-name-face ((t (:foreground ,mince-fn))))
+ `(font-lock-builtin-face ((t (:foreground ,mince-fg1))))
+ `(font-lock-constant-face ((t (:foreground ,mince-fg1))))
+ `(font-lock-keyword-face ((t (:foreground ,mince-mid))))
+ `(font-lock-type-face ((t (:foreground ,mince-types))))
+ 
+ ;; string-comment-like
+ `(font-lock-comment-face ((t (:foreground ,mince-comment))))
+ `(font-lock-doc-face ((t (:foreground ,mince-comment))))
+ `(rust-builtin-formatting-macro ((t (:foreground ,mince-macro))))
+ `(font-lock-preprocessor-face ((t (:foreground ,mince-macro))))
+ `(font-lock-string-face ((t (:foreground ,mince-string))))
+
+ ;; cursor
+ '(cursor ((t (:background "#8ea1ad"))))
+
+ `(font-lock-variable-name-face ((t (:foreground ,mince-vars))))
+ '(font-lock-warning-face ((t (:foreground "red" :bold t))))
+ '(fringe ((t (:background "#3d3f3f"))))
+ '(minibuffer-prompt ((t (:foreground "#1ea3df" :bold t))))
+ '(mode-line ((t (:foreground "#b8b8b8" :background "#515151"))))
+ '(region ((t (:background "#3c3c3c"))))
+ '(rust-ampersand-face ((t (:foreground "#b8c6c6"))))
+ 
+ '(secondary-selection ((t (:background "#111111"))))
+ '(shadow ((t (:foreground "#4e5a63"))))
+ '(show-paren-match ((t (:background nil))))
+ )
 
 
